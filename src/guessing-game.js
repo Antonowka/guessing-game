96b1dcdaba7959213a9e0619fd91b9fa0c803f3c
Antonowka <navickiy1994@gmail.com>
2022-08-16 13:48:01 +0200

    feat: added solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	constructor() {
+		this.num = Math.ceil(this.max / 2);
+		this.min;
+		this.max;
+	}
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+	}
+	guess() {
+		this.num = Math.ceil((this.max - this.min) / 2 + this.min);
+		return this.num;
+	}
+	lower() {
+		this.setRange(this.min, this.num);
+	}
+	greater() {
+		this.setRange(this.num, this.max);
+	}
 }
 
 module.exports = GuessingGame;
